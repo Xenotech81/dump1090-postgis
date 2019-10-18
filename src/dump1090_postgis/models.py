15da689d88590629c6d63c23b94bf4745b16b7d9
Xenotech81 <56318007+Xenotech81@users.noreply.github.com>
2019-10-18 22:39:00 +0200

    Added models package. Created first data model 'Flight' which updates from AdsbMessage iterator instance.

---
+++ src/dump1090_postgis/models.py
@@ -0,0 +1,49 @@
+# from geoalchemy2 import Geometry
+import logging
+import string
+
+import adsb_parser
+
+log = logging.getLogger(__name__)
+
+
+class Flight(object):
+
+    def __init__(self, hexident: string):
+        self.hexident = hexident
+        # Time stamp of last received message
+        self.last_seen = None
+        # Last position (not necessarily from last message)
+        self.position = None
+        self.verticalrate = None
+        self.squawk = None
+        self.onground = True
+        self.__flight_track_positions = []
+        self.__flight_track_timestamps = []
+        self._transmission_type_count = dict.fromkeys(range(1, 9, 1), 0)
+
+    def update(self, adsb: adsb_parser.AdsbMessage):
+        """
+        Updates the instance attributes with values from an ADSb message object.
+        :param adsb: Instance of AdsbMessage
+        """
+        self._transmission_type_count[adsb.transmission_type] += 1
+
+        self.hexident = adsb.hexident
+        self.last_seen = adsb.gen_date + adsb.gen_time
+        self.position = (adsb.latitude, adsb.longitude, adsb.altitude)
+        self.verticalrate = adsb.verticalrate
+        self.onground = adsb.onground
+
+        # Update only if msg includes coordinates
+        self.__flight_track_positions.append(self.position)
+        self.__flight_track_timestamps.append(self.last_seen)
+
+
+if __name__ == '__main__':
+    flight = Flight('dummy')
+
+    message_source = adsb_parser.FileSource('messages.txt')
+    for msg in adsb_parser.AdsbMessage(message_source):
+        flight.update(msg)
+        log.info(flight.__dict__)
