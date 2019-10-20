6b8af9b72fa0259aac965a9ede02d1ff0dfff343
Xenotech81 <56318007+Xenotech81@users.noreply.github.com>
2019-10-20 21:49:16 +0200

    Added getter, setter, iter methods to CurrentFlights

--- src/dump1090_postgis/flights.py
+++ src/dump1090_postgis/flights.py
@@ -4,7 +4,6 @@ import string
 from models import Flight
 import adsb_parser
 
-logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
@@ -22,6 +21,24 @@ class CurrentFlights(object):
         # Key-value pairs of fight hexident and models.Flight instances
         self._flights = {}
 
+    def __getitem__(self, hexident: string) -> Flight:
+        try:
+            return self._flights[hexident]
+        except KeyError:
+            log.error("Cannot find flight {} in current flights pool. Not on the radar...".format(hexident))
+            return None
+
+    def __setitem__(self, key, value):
+        assert isinstance(value, Flight)
+        self._flights[key] = value
+
+    def __iter__(self):
+        for flight in self._flights:
+            yield flight
+
+    def __len__(self):
+        return  len(self._flights)
+
     def update(self, adsb_message: adsb_parser.AdsbMessage):
         """
         Updates the flight pool from a ADSb message object.
@@ -36,7 +53,7 @@ class CurrentFlights(object):
             self._flights[adsb_message.hexident] = Flight(adsb_message.hexident).update(adsb_message)
 
     def __repr__(self):
-        return "Current flight pool contains {} fights: \n{}".format(len(self._flights), '\n'.join(self.hexidents()))
+        return "Current flight pool contains {} fights: \n{}".format(len(self), '\n'.join(self.hexidents()))
 
     def hexidents(self):
         return self._flights.keys()
@@ -44,12 +61,14 @@ class CurrentFlights(object):
 
 if __name__ == '__main__':
 
+    logging.basicConfig(level=logging.INFO)
+
     # Pool of currently visible flights
     current_flights = CurrentFlights()
 
-    message_source = adsb_parser.FileSource('messages_long.txt')
+    message_source = adsb_parser.FileSource('messages.txt')
     for msg in adsb_parser.AdsbMessage(message_source):
         current_flights.update(msg)
 
     log.info(current_flights)
-    log.info(list(current_flights._flights['396444'].flight_track()))
+    log.info(list(current_flights['400BE5'].flight_track()))
