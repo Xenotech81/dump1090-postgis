e90d05db11634cfb68035b0910d75793cc33084c
Xenoman <alexradi81@gmail.com>
2020-02-25 12:18:58 +0100

    Added handling of SIGTERM sent by docker stop command for a graceful shuitdown of the container

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 import logging
+import signal
 import sys
 
+from dbmanager import session
 from dump1090_postgis.adsb_parser import AdsbMessageFilter, AdsbMessage, Dump1090Socket
 from dump1090_postgis.flights import CurrentFlights
 
@@ -8,13 +10,24 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
+def handle_sigterm():
+    """Handle Docker's SIGTERM by raising KeyboardInterrupt."""
+
+    def exit_gracefully(signum, frame):
+        raise KeyboardInterrupt
+
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+
 def main():
     log.info(">>> WELCOME TO THE ADSB POSTGIS LOGGER <<<")
 
-    from dbmanager import session
+    handle_sigterm()
+
     current_flights = CurrentFlights(session=session, adsb_filter=AdsbMessageFilter(below=10000))
 
     message_source = Dump1090Socket()
+
     log.info("Start logging messages...")
     try:
         for msg in AdsbMessage(message_source):
