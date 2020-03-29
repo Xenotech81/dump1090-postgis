3425282ed033541acdebe8fc788ff6b2a494be81
Xenoman <alexradi81@gmail.com>
2020-03-29 22:48:17 +0200

    Extended interpolate_track unit tests

--- src/dump1090_postgis/shared.py
+++ src/dump1090_postgis/shared.py
@@ -16,7 +16,7 @@ def deg2angle(deg):
         return angle
 
 
-def angle2geg(deg):
+def angle2deg(deg):
     """Transforms mathematical angle in a x-y plane to wind direction in degrees [0...360]."""
     angle = 270.0 - deg
     if angle > 360:
@@ -25,12 +25,7 @@ def angle2geg(deg):
         return angle
 
 
-def angle2deg(angle):
-    """Transform mathematical angle to compass direction."""
-    return (450 - angle) % 360
-
-
-def interpolate_track(positions: iter, poly_order: int = 1):
+def interpolate_track(positions: iter):
     """Compute flight heading from a list of points by least squares polynomial fit.
 
     :param positions: List of Position instances to interpolate the track from
@@ -38,6 +33,6 @@ def interpolate_track(positions: iter, poly_order: int = 1):
     """
     points = np.array(LineString([p.point for p in positions]))
     rad = np.arctan2(np.diff(points[..., 1]), np.diff(points[..., 0]))
-    angle = np.rad2deg(rad)
+    angle = np.rad2deg(rad)[0]
 
-    return angle2geg(angle[0])
+    return angle2deg(angle)
--- src/tests/test_shared.py
+++ src/tests/test_shared.py
@@ -1,28 +1,32 @@
 import unittest
 
+from unittest.mock import Mock
 from shapely.geometry import Point
 
-from dump1090_postgis.shared import angle2deg, interpolate_track, angle2geg
+from dump1090_postgis.shared import interpolate_track, angle2deg
 
 
 class TestFunctions(unittest.TestCase):
 
-    @unittest.skip("")
-    def test_angle2compass(self):
-        #self.assertEqual(angle2deg(0), 270)
-        self.assertEqual(angle2deg(90), 180)
-
-    def test_angle2geg(self):
-        self.assertAlmostEqual(angle2geg(0), 270, 3)
-        self.assertAlmostEqual(angle2geg(45), 225, 3)
-        self.assertAlmostEqual(angle2geg(180), 90, 3)
-        self.assertAlmostEqual(angle2geg(-180), 90, 3)
-        self.assertAlmostEqual(angle2geg(-89), 359, 3)
+    def test_angle2deg(self):
+        self.assertAlmostEqual(angle2deg(0), 270, 3)
+        self.assertAlmostEqual(angle2deg(45), 225, 3)
+        self.assertAlmostEqual(angle2deg(180), 90, 3)
+        self.assertAlmostEqual(angle2deg(-180), 90, 3)
+        self.assertAlmostEqual(angle2deg(-89), 359, 3)
 
     def test_interpolate_track(self):
-        self.assertAlmostEqual(interpolate_track([Point(0, 0), Point(-1, 0)]), 90, 3)  # East wind
-        self.assertAlmostEqual(interpolate_track([Point(0, 0), Point(0, 1)]), 180, 3)  # South wind
-        self.assertAlmostEqual(interpolate_track([Point(0, 0), Point(-1, -1)]), 45, 3)  # Northest wind
+        # East wind
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(0, 0)), Mock(point=Point(-1, 0))]), 90, 3)
+        # South wind
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(0, 0)), Mock(point=Point(0, 1))]), 180, 3)
+        # Northest wind
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(0, 0)), Mock(point=Point(-1, -1))]), 45, 3)
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(1, 1)), Mock(point=Point(-1, -1))]), 45, 3)
+        # Northest wind
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(0, 0)), Mock(point=Point(1, 1))]), 225, 3)
+        self.assertAlmostEqual(interpolate_track([Mock(point=Point(-1, -1)), Mock(point=Point(0, 0))]), 225, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
