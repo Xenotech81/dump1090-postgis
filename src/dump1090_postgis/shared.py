89d56c3b1e8bff964c8fdebd21414da3bfa6a1ba
Xenoman <alexradi81@gmail.com>
2020-07-02 14:07:33 +0200

    Bugfix: Using now the last (not the first!) two positions to interpolate the flight heading.

--- src/dump1090_postgis/shared.py
+++ src/dump1090_postgis/shared.py
@@ -34,18 +34,17 @@ def angle2compass(deg):
 
 
 def interpolate_track(positions: iter):
-    """Compute flight heading from a list of two aircraft positions.
+    """Compute flight heading from a list of two or more aircraft positions.
 
-    Positions is a list of 2 or more Points, sorted in ascending order
-    (chronologically along the flight path).
-
-    NOTE: For now no track is interpolated, just a local heading from the first two points in the list of points.
+    Expects a list of 2 or more positions, sorted in chronologically ascending order along the flight path. The
+    track heading is computed from the last two points in the list.
 
     :param positions: List of Position instances to interpolate the track from
-    :type positions: list of shapely.geometry.Point
+    :type positions: list of models.Position
     """
+
     points = np.array(LineString([p.point for p in positions]))
     rad = np.arctan2(np.diff(points[..., 1]), np.diff(points[..., 0]))
-    deg = np.rad2deg(rad)[0]
+    deg = np.rad2deg(rad)[-1]  # Take only the heading interpolation of the last 2 positions
 
     return angle2compass(deg)
